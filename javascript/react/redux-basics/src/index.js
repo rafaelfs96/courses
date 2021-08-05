b80d8876a416400c66af332cf04e30bfdd5f4f62
rafaelfs96 <rafaelfs96@outlook.com>
2021-08-05 13:33:48 -0300

    redux-basics

---
+++ javascript/react/redux-basics/src/index.js
@@ -0,0 +1,19 @@
+import './index.sass'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+import { Provider } from 'react-redux'
+import configStore from './store/storeConfig'
+
+const store = configStore()
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+)
