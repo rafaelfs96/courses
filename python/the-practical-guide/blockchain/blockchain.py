ceb97fd92d5f095e80fa308496a059d932ded2d9
rafaelfs96 <rafaelfs96@outlook.com>
2021-08-15 01:38:57 -0300

    python blockchain

--- python/the-practical-guide/blockchain/blockchain.py
+++ python/the-practical-guide/blockchain/blockchain.py
@@ -24,25 +24,41 @@ def add_transaction(transaction_amount, last_transaction=[1]):
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
-    user_input = float(input('your transaction amount: '))
-    return user_input
+    return float(input('your transaction amount: '))
 
 
 def get_user_choice():
-    user_input = input('your choice: ')
-    return user_input
+    return input('your choice: ')
 
 
 def print_blockchain_elements():
     for block in blockchain:
-        print('outputting block')
         print(block)
+    else:
+        print('-' * 20)
 
 
-while True:
+def verify_chain():
+    is_valid = True
+
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    return is_valid
+
+
+waiting_for_input = True
+
+while waiting_for_input:
     print('please choose')
     print('1: add a new transaction value')
     print('2: output the blockchain blocks')
+    print('h: manipulate the chain')
     print('q: Quit')
 
     user_choice = get_user_choice()
@@ -51,9 +67,18 @@ while True:
         add_transaction(get_transaction_value(), get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('input was invalid, please pick a value from the list')
+
+    if not verify_chain():
+        print('Invalid Blockchain!')
+        break
+else:
+    print('user left')
 
 print('done')
