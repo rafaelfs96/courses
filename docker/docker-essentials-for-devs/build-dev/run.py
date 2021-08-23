360c59f9cb7895e0a7cb2685f95258fd98b7e311
rafaelfs96 <rafaelfs96@outlook.com>
2021-08-23 10:27:51 -0300

    docker

---
+++ docker/docker-essentials-for-devs/build-dev/run.py
@@ -0,0 +1,29 @@
+import logging
+import http.server
+import socketserver
+import getpass
+
+
+class MyHttpHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        logging.info("%s - - [%s] $s\n" % (
+            self.client_address[0],
+            self.log_date_time_string(),
+            format%args
+        ))
+
+
+logging.basicConfig(
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('inicializando...')
+PORT = 8000
+
+httpd = socketserver.TCPServer(('', PORT), MyHttpHandler)
+logging.info('Escutando a porta: %s', PORT)
+logging.info('Usuario: %s', getpass.getuser())
+httpd.serve_forever()
